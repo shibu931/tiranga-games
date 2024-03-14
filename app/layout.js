588e3f68392e8dba333cbd9509e734d0fcc0a4cb
Shippu Kumar <Itzshippu@gmail.com>
2024-03-15 00:27:59 +0530

    dsgsd

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,18 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Tiranga Games',
   description: 'Experience the adrenaline rush of color prediction gaming with Trianga Game! Predict colors in real-time, compete globally, and win big. Secure transactions and seamless gameplay await. Join us for an exhilarating journey to unlock fortunes!',
+  generator: 'Tiranga Games',
+  applicationName: 'Tiranga Games',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Tiranga Games', 'Color Prediction Game', 'Daman Game','91Club',"avitator game",'color trading','bounty games'],
+  authors: [{ name: 'Rahul Kumar' }, { name: 'Rahul Kumar', url: 'https://tiranga-games.vercel.app/' }],
+  creator: 'Rahul Kumar',
+  publisher: 'Tiranga Game App',
+  formatDetection: {
+    email: 'tirangagames@gmail.com',
+    address: false,
+    telephone: false,
+  },
 }
 
 export default function RootLayout({ children }) {
