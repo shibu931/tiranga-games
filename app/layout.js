95ccefda363e7b3305f1d0f9d7d65fe717be0852
Shippu Kumar <Itzshippu@gmail.com>
2024-03-15 00:40:34 +0530

    DASFJKDbsjk

--- app/layout.js
+++ app/layout.js
@@ -21,6 +21,47 @@ export const metadata = {
     address: false,
     telephone: false,
   },
+  metadataBase: new URL('https://tiranga-games.vercel.app/'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'Uncovering the Colors of Triumph: Aviator Gaming & Tiranga Color Prediction - Your Passport to Excitement and Benefits!',
+    description: 'Enter the thrilling world of Aviator Gaming and Tiranga Color Prediction, where strategy and excitement collide. Savor the thrill of making color predictions and navigating thrilling adventures as you compete for amazing prizes. Come play with us for a unique gaming experience where every choice you make could result in success. Are you prepared to accept the challenge and take off?',
+    url: 'https://tiranga-games.vercel.app/',
+    siteName: 'Tiranga Games',
+    images: [
+      {
+        url: 'https://tiranga-games.vercel.app/og-img.png', 
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Tiranga Game Color prediction color trading aviator',
+    description: '🚀 Enter the world of Tiranga Color Prediction & Aviator Gaming! 🎮 Dive into thrilling predictions and high-flying adventures for your shot at victory and rewards. Are you ready to take on the challenge? #TirangaGaming #Aviator #PredictAndWin',
+    siteId: '1467726470533754880',
+    creator: '@tirangagame',
+    creatorId: '1467726470533754880',
+    images: ['https://tiranga-games.vercel.app/og-img.png'], // Must be an absolute URL
+  },
 }
 
 export default function RootLayout({ children }) {
