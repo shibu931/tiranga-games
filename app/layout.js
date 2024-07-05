4df6f67bfa2b57452c4e9da2d60cb0da08908b28
Shippu Kumar <Itzshippu@gmail.com>
2024-07-05 17:14:12 +0530

    fdsngk

--- app/layout.js
+++ app/layout.js
@@ -70,8 +70,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-N62SSS7YJZ"></Script>
-        <Script>
+        <Script>{
+          `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'scriptScriptdataLayer','GTM-MJ3DDWPZ');`
+        }</Script>
+        <meta name="google-site-verification" content="96_2mskKLZeyue1Tej37YFmmV3HufLrY0SWcrH5Nt6E" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-N62SSS7YJZ"></Script>
+        {/* <Script>
           {
             `  window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
@@ -80,9 +88,11 @@ export default function RootLayout({ children }) {
       gtag('config', 'G-N62SSS7YJZ');
     `
           }
-        </Script>
+        </Script> */}
       </head>
       <body className={inter.className + 'w-svw h-svh relative'}>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MJ3DDWPZ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Navbar />
         {children}
         <div className='fixed bottom-5 right-10 animate-bounce '>
--- components/shared/DialogComponent.jsx
+++ components/shared/DialogComponent.jsx
@@ -1,13 +1,4 @@
 "use client"
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
-import Link from "next/link"
 import { useEffect, useState } from "react"
 
 
